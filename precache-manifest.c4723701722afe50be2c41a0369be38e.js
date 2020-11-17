bb4a53c2aa5340a064222f7069f42978f0c98ed3
Asim <asim.widefy>
2020-11-17 14:04:18 +0530

    Updates

---
+++ precache-manifest.c4723701722afe50be2c41a0369be38e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28b0d1d7adef94ccff499c6429fd21ae",
+    "url": "/invoice/index.html"
+  },
+  {
+    "revision": "225491dfd2395641d688",
+    "url": "/invoice/static/css/main.27622ffd.chunk.css"
+  },
+  {
+    "revision": "133b5297066f5b635678",
+    "url": "/invoice/static/js/2.a88709ae.chunk.js"
+  },
+  {
+    "revision": "88011752e4ff9ce31dfb431496cb8e29",
+    "url": "/invoice/static/js/2.a88709ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "225491dfd2395641d688",
+    "url": "/invoice/static/js/main.79227b18.chunk.js"
+  },
+  {
+    "revision": "8f56036bf52aead71f45",
+    "url": "/invoice/static/js/runtime-main.40dc4efe.js"
+  },
+  {
+    "revision": "fb235184b1c260dc0407b5b22671fc7a",
+    "url": "/invoice/static/media/edigital_wolf_logo.fb235184.png"
+  }
+]);
